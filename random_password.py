7b6c2f24f0ef8769e2c8c8f72915a6e586f58412
Amin-Hallaj <aminhallajm@gmail.com>
2024-06-27 23:30:29 +0330

    Save the characters in the list

--- random_password.py
+++ random_password.py
@@ -2,3 +2,10 @@
 
 import random
 import string
+
+
+#Save the characters in the list
+srt1 = list(string.ascii_lowercase)
+srt2 = list(string.ascii_uppercase)
+srt3 = list(string.digits)
+srt4 = list(string.punctuation)
