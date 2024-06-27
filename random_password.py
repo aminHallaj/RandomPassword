96dbf2fc24ea7dfeb03602e8f0f8078f7ed6bfc4
Amin-Hallaj <aminhallajm@gmail.com>
2024-06-27 23:32:21 +0330

    Entering the number of passwords from users

--- random_password.py
+++ random_password.py
@@ -4,8 +4,12 @@ import random
 import string
 
 
-#Save the characters in the list
+# Save the characters in the list
 srt1 = list(string.ascii_lowercase)
 srt2 = list(string.ascii_uppercase)
 srt3 = list(string.digits)
 srt4 = list(string.punctuation)
+
+
+# Entering the number of passwords from users
+input = input("How many characters do you want in your password?")
